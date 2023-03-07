2b52b62e275a58294e2745328bdb9aa74a1d0c92
Konstantin Balakirev <kbalakirev@gmail.com>
2023-03-07 11:28:28 -0600

    added arrow functionality

--- client/components/PageContent.js
+++ client/components/PageContent.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { IoChevronBackCircle } from 'react-icons/io5';
+import { IoChevronForwardCircle } from 'react-icons/io5';
 
 export default function PageContent() {
 	function arrowRight() {
@@ -6,32 +8,46 @@ export default function PageContent() {
 		journeyScroller.scrollLeft += screen.width - 275;
 	}
 
+	function arrowLeft() {
+		let journeyScroller = document.getElementById('journey-scroller');
+		journeyScroller.scrollLeft -= screen.width - 275;
+	}
+
 	return (
 		<div className='page-content'>
-			{/* <div className='content-window'></div> */}
-			<div id='journey-scroller' className='content-scroller'>
+			<div className='content-wrapper'>
 				<div
-					id='arrow-right'
+					className='content-arrow-left'
+					onClick={() => {
+						arrowLeft();
+					}}
+				>
+					<IoChevronBackCircle className='arrow-icon' />
+				</div>
+				<div id='journey-scroller' className='content-scroller'>
+					<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
+					<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
+					<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
+					<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
+					<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
+					{/* <img src='C:\Users\Kostya\Projects Work Bench\Graphics\kostya-works\test content.png' />
+				<img src='C:\Users\Kostya\Projects Work Bench\Graphics\kostya-works\test content.png' />
+				<img src='C:\Users\Kostya\Projects Work Bench\Graphics\kostya-works\test content.png' />
+				<img src='C:\Users\Kostya\Projects Work Bench\Graphics\kostya-works\test content.png' /> */}
+					{/* <img src='https://media.gq.com/photos/5b6b20e3a3a1320b7280f029/16:9/w_1600/The-Brutal-Wonders-Of-The-Architecture-World-GQ-Style-Fall-2018_07.jpg' />
+				<img src='https://architecturesideas.com/wp-content/uploads/2018/02/modern-architecture-buildings-9.jpg' />
+				<img src='https://cdn.contexttravel.com/image/upload/c_fill,q_60,w_2400/hyowdxuvgsqzb4ngaqgq.jpg' /> */}
+				</div>
+				<div
+					className='content-arrow-right'
 					onClick={() => {
 						arrowRight();
 					}}
 				>
-					arrow right
+					<IoChevronForwardCircle className='arrow-icon' />
 				</div>
-
-				<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
-				<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
-				<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
-				<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
-				<img src='https://storage.googleapis.com/kostya-works-public/design/test%20content.png' />
-				{/* <img src='C:\Users\Kostya\Projects Work Bench\Graphics\kostya-works\test content.png' />
-				<img src='C:\Users\Kostya\Projects Work Bench\Graphics\kostya-works\test content.png' />
-				<img src='C:\Users\Kostya\Projects Work Bench\Graphics\kostya-works\test content.png' />
-				<img src='C:\Users\Kostya\Projects Work Bench\Graphics\kostya-works\test content.png' /> */}
-				{/* <img src='https://media.gq.com/photos/5b6b20e3a3a1320b7280f029/16:9/w_1600/The-Brutal-Wonders-Of-The-Architecture-World-GQ-Style-Fall-2018_07.jpg' />
-				<img src='https://architecturesideas.com/wp-content/uploads/2018/02/modern-architecture-buildings-9.jpg' />
-				<img src='https://cdn.contexttravel.com/image/upload/c_fill,q_60,w_2400/hyowdxuvgsqzb4ngaqgq.jpg' /> */}
 			</div>
+
 			<div id='navigation' className='content-navigation'>
 				CONTENT NAVIGATION
 			</div>
