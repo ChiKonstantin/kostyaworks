f5427afe2c4299c0e1f18b5b4d2aa7d844fdedcd
Konstantin Balakirev <kbalakirev@gmail.com>
2023-03-21 11:25:05 -0500

    consolidated page content functions in one file

--- client/components/JourneyContent.js
+++ client/components/JourneyContent.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { IoChevronBackCircle } from 'react-icons/io5';
-import { IoChevronForwardCircle } from 'react-icons/io5';
+
 import NavMarker from './NavMarker';
 import { journeyData } from '../contentData';
 import ContentModule from './ContentModule';
+import {
+	returnNavArr,
+	activateContentSlide,
+	arrowScroll,
+	navStatus,
+	arrowStatus,
+	renderLeftArrow,
+	renderRightArrow,
+} from '../functions';
 
 export default function JourneyContent() {
 	//update these:
@@ -13,13 +21,12 @@ export default function JourneyContent() {
 
 	const [arrowState, setArrows] = useState({ left: false, right: true });
 	const [activeNav, setActiveNav] = useState(0);
-	// const [contentNavObjStatus, setContentNavObjStatus] = useState(false);
 	const [navArr, setNavArr] = useState([{ index: 0, startLocation: 0 }]);
 	const [subsectionsCount, setSubsectionsCount] = useState(0);
 
 	useEffect(() => {
 		if (navArr.length === 1) {
-			returnNavArr(contentData);
+			returnNavArr(contentData, pageName, setNavArr);
 		}
 
 		if (subsectionsCount === 0) {
@@ -31,148 +38,34 @@ export default function JourneyContent() {
 		}
 	});
 
-	function returnNavArr(contentArr) {
-		let scroller = document.getElementById(`${pageName}-scroller`);
-		let scrollerStyle = document.defaultView.getComputedStyle(scroller);
-
-		let arr = [];
-		//scroller width is equal to module width
-		let moduleWidth = parseInt(scrollerStyle.width);
-		let locationOffset = 0;
-
-		contentArr.map((section) => {
-			const sectionIndex = contentArr.indexOf(section);
-			const sectionStartLocation = locationOffset;
-
-			arr.push({
-				index: sectionIndex,
-				startLocation: sectionStartLocation,
-			});
-
-			locationOffset =
-				locationOffset + moduleWidth * section.subsections.length;
-		});
-
-		setNavArr(arr);
-	}
-
-	function activateContentSlide(section, activeNav) {
-		if (contentData.indexOf(section) === activeNav) {
-			return 'content-slide active';
-		} else {
-			return 'content-slide';
-		}
-	}
-
-	function arrowScroll(direction, sectionsArr) {
-		let scroller = document.getElementById(`${pageName}-scroller`);
-		let scrollerStyle = document.defaultView.getComputedStyle(scroller);
-		//scroller width is equal to module width
-		let scrollerWidth = parseInt(scrollerStyle.width);
-
-		if (direction === 'left' && scroller.scrollLeft > 0) {
-			scroller.scrollLeft -= scrollerWidth;
-		}
-
-		if (
-			direction === 'right' &&
-			scroller.scrollLeft < scrollerWidth * (subsectionsCount - 1)
-		) {
-			scroller.scrollLeft += scrollerWidth;
-		}
-	}
-
-	function navStatus(arr) {
-		let scroller = document.getElementById(`${pageName}-scroller`);
-		let scrollerStyle = document.defaultView.getComputedStyle(scroller);
-		//scroller width is equal to module width
-		let scrollerWidth = parseInt(scrollerStyle.width);
-
-		// const activeNavNumber = Math.floor(
-		// 	(scroller.scrollLeft + scrollValue / 2) / (scrollValue * 2)
-		// );
-
-		for (let i = arr.length - 1; i >= 0; i--) {
-			// console.log('', arr[i]);
-			if (scroller.scrollLeft + scrollerWidth / 2 >= arr[i].startLocation) {
-				// console.log(arr[i].index);
-				setActiveNav(arr[i].index);
-
-				break;
-			}
-		}
-	}
-
-	// function sectionContent(arr, data) {
-	// 	const contentsArray = arr;
-
-	// 	let scroller = document.getElementById(`${pageName}-scroller`);
-	// 	let scrollerStyle = document.defaultView.getComputedStyle(scroller);
-	// 	let scrollerWidth = parseInt(scrollerStyle.width);
-	// 	for (let i = arr.length - 1; i >= 0; i--) {
-	// 		// console.log('', arr[i]);
-	// 		if (scroller.scrollLeft >= arr[i].startLocation) {
-	// 			// console.log(arr[i].index);
-	// 			setActiveNav(arr[i].index);
-	// 			break;
-	// 		}
-	// 	}
-	// }
-
-	function arrowStatus() {
-		let scroller = document.getElementById(`${pageName}-scroller`);
-		let scrollerStyle = document.defaultView.getComputedStyle(scroller);
-		//scroller width is equal to module width
-		let scrollerWidth = parseInt(scrollerStyle.width);
-
-		if (scroller.scrollLeft === 0) {
-			setArrows(() => ({ left: false, right: true }));
-		}
-		if (
-			0 < scroller.scrollLeft &&
-			scroller.scrollLeft < scrollerWidth * (subsectionsCount - 1)
-		) {
-			setArrows(() => ({ left: true, right: true }));
-		}
-		if (scroller.scrollLeft >= scrollerWidth * (subsectionsCount - 1)) {
-			setArrows(() => ({ left: true, right: false }));
-		}
-	}
-
-	function renderLeftArrow() {
-		if (arrowState.left) {
-			return <IoChevronBackCircle className='arrow-icon active' />;
-		} else {
-			return <IoChevronBackCircle className='arrow-icon inactive' />;
-		}
-	}
-
-	function renderRightArrow() {
-		if (arrowState.right) {
-			return <IoChevronForwardCircle className='arrow-icon active' />;
-		} else {
-			return <IoChevronForwardCircle className='arrow-icon inactive' />;
-		}
-	}
-
 	return (
 		<div className='page-content'>
 			{contentData.map((section) => {
 				return (
 					<div
-						className={activateContentSlide(section, activeNav)}
+						className={activateContentSlide(section, activeNav, contentData)}
 						style={{
 							backgroundImage: `url(${section.imageUrl})`,
 						}}
-					></div>
+					>
+						{/* <video
+							preload='auto'
+							style={{ height: '100%', width: 'auto', alignItems: 'center' }}
+							src='https://storage.googleapis.com/kostya-works-public/design/S1550015.MP4'
+							// controls
+							autoPlay
+							muted
+							loop
+						></video> */}
+					</div>
 				);
 			})}
 			<div
 				id={pageName + '-scroller'}
 				className='content-scroller'
 				onScroll={() => {
-					arrowStatus();
-					navStatus(navArr);
+					arrowStatus(pageName, subsectionsCount, setArrows);
+					navStatus(navArr, pageName, setActiveNav);
 
 					// sectionContent(navArr, contentData);
 				}}
@@ -194,10 +87,10 @@ export default function JourneyContent() {
 					<div
 						className='arrow-wrapper'
 						onClick={() => {
-							arrowScroll('left', contentData);
+							arrowScroll('left', pageName, subsectionsCount);
 						}}
 					>
-						{renderLeftArrow()}
+						{renderLeftArrow(arrowState.left)}
 					</div>
 				</div>
 
@@ -221,10 +114,10 @@ export default function JourneyContent() {
 					<div
 						className='arrow-wrapper'
 						onClick={() => {
-							arrowScroll('right', contentData);
+							arrowScroll('right', pageName, subsectionsCount);
 						}}
 					>
-						{renderRightArrow()}
+						{renderRightArrow(arrowState.right)}
 					</div>
 				</div>
 			</div>
--- client/functions.js
+++ client/functions.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { IoChevronBackCircle } from 'react-icons/io5';
+import { IoChevronForwardCircle } from 'react-icons/io5';
+
 export function moveBarOnScroll() {
 	let selectBar = document.getElementById('menu-select-bar');
 	let sideMenu = document.getElementById('side-menu');
@@ -11,4 +15,111 @@ export function moveBarOnScroll() {
 		(scrollPosition / (screenHeight * 4)) * parseInt(sideMenuStyle.height);
 
 	selectBar.style.top = `${parseInt(sideMenuStyle.top) + scrollRatio}px`;
+}
+
+export function returnNavArr(contentArr, pageName, setNavArr) {
+	let scroller = document.getElementById(`${pageName}-scroller`);
+	let scrollerStyle = document.defaultView.getComputedStyle(scroller);
+
+	let arr = [];
+	//scroller width is equal to module width
+	let moduleWidth = parseInt(scrollerStyle.width);
+	let locationOffset = 0;
+
+	contentArr.map((section) => {
+		const sectionIndex = contentArr.indexOf(section);
+		const sectionStartLocation = locationOffset;
+
+		arr.push({
+			index: sectionIndex,
+			startLocation: sectionStartLocation,
+		});
+
+		locationOffset = locationOffset + moduleWidth * section.subsections.length;
+	});
+
+	setNavArr(arr);
+}
+
+export function activateContentSlide(section, activeNav, contentData) {
+	if (contentData.indexOf(section) === activeNav) {
+		return 'content-slide active';
+	} else {
+		return 'content-slide';
+	}
+}
+
+export function arrowScroll(direction, pageName, subsectionsCount) {
+	let scroller = document.getElementById(`${pageName}-scroller`);
+	let scrollerStyle = document.defaultView.getComputedStyle(scroller);
+	//scroller width is equal to module width
+	let scrollerWidth = parseInt(scrollerStyle.width);
+
+	if (direction === 'left' && scroller.scrollLeft > 0) {
+		scroller.scrollLeft -= scrollerWidth;
+	}
+
+	if (
+		direction === 'right' &&
+		scroller.scrollLeft < scrollerWidth * (subsectionsCount - 1)
+	) {
+		scroller.scrollLeft += scrollerWidth;
+	}
+}
+
+export function navStatus(arr, pageName, setActiveNav) {
+	let scroller = document.getElementById(`${pageName}-scroller`);
+	let scrollerStyle = document.defaultView.getComputedStyle(scroller);
+	//scroller width is equal to module width
+	let scrollerWidth = parseInt(scrollerStyle.width);
+
+	// const activeNavNumber = Math.floor(
+	// 	(scroller.scrollLeft + scrollValue / 2) / (scrollValue * 2)
+	// );
+
+	for (let i = arr.length - 1; i >= 0; i--) {
+		// console.log('', arr[i]);
+		if (scroller.scrollLeft + scrollerWidth / 2 >= arr[i].startLocation) {
+			// console.log(arr[i].index);
+			setActiveNav(arr[i].index);
+
+			break;
+		}
+	}
+}
+
+export function arrowStatus(pageName, subsectionsCount, setArrows) {
+	let scroller = document.getElementById(`${pageName}-scroller`);
+	let scrollerStyle = document.defaultView.getComputedStyle(scroller);
+	//scroller width is equal to module width
+	let scrollerWidth = parseInt(scrollerStyle.width);
+
+	if (scroller.scrollLeft === 0) {
+		setArrows(() => ({ left: false, right: true }));
+	}
+	if (
+		0 < scroller.scrollLeft &&
+		scroller.scrollLeft < scrollerWidth * (subsectionsCount - 1)
+	) {
+		setArrows(() => ({ left: true, right: true }));
+	}
+	if (scroller.scrollLeft >= scrollerWidth * (subsectionsCount - 1)) {
+		setArrows(() => ({ left: true, right: false }));
+	}
+}
+
+export function renderLeftArrow(direction) {
+	if (direction) {
+		return <IoChevronBackCircle className='arrow-icon active' />;
+	} else {
+		return <IoChevronBackCircle className='arrow-icon inactive' />;
+	}
+}
+
+export function renderRightArrow(direction) {
+	if (direction) {
+		return <IoChevronForwardCircle className='arrow-icon active' />;
+	} else {
+		return <IoChevronForwardCircle className='arrow-icon inactive' />;
+	}
 }
